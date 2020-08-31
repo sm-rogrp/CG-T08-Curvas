dc36815ebb58d6dadb3f1aa7e7c1b2f6ed0221b8
rogrp <yihsic@gmail.com>
2020-08-31 14:50:17 -0500

    change configuration

--- T08-App-Curvas/src/main.cpp
+++ T08-App-Curvas/src/main.cpp
@@ -1,8 +1,8 @@
-#include "imgui\imgui.h"
-#include "imgui\imgui_impl_glfw.h"
-#include "imgui\imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-#include "..\irrklang\irrKlang.h"
+#include "irrKlang.h"
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
